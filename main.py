645ba13f7be6876679ac43d51dba00b39a06bf9c
avidalto <avidalto@hawk.iit.edu>
2023-04-14 13:29:08 -0500

    Run sbach instead of srun

--- main.py
+++ main.py
@@ -12,11 +12,38 @@ from parsl_utils.data_provider import PWFile
 
 from workflow_apps import compile_mpi_hello_world_ompi, run_mpi_hello_world_ompi
 
+from dataclasses import dataclass, fields
+from typing import Optional
+
+@dataclass
+class SbatchOptions:
+    account: Optional[str] = None
+    nodes: Optional[int] = None
+    job_name: Optional[str] = None
+    ntasks_per_node: Optional[int] = None
+    output: Optional[str] = None
+    partition: Optional[str] = None
+    
+    @property
+    def header(self):
+        option_names = [f.name for f in fields(self)]
+        header_str = ''
+        for oname in option_names:
+            oval = getattr(self, oname)
+            if oval:
+                header_str += '\nSBATCH --' + oname.replace('_','-') + '=' + str(oval) 
+
+        return header_str
+
 
 if __name__ == '__main__':
     print('Loading Parsl Config', flush = True)
     parsl.load(config)
     args = read_args()
+    # Dictionary with only the slurm parameters. These are the parameters that start with slurm_
+    # in the workflow.xml file. The slurm_ part is removed so that {'slurm_nodes': 1} becomes
+    # {'nodes': 1}
+    slurm_args = {k.replace("slurm_", ""): v for k, v in args.items() if k.startswith("slurm_")}
 
     print('\n\nCompiling test', flush = True)
     compile_fut = compile_mpi_hello_world_ompi(
@@ -32,8 +59,15 @@ if __name__ == '__main__':
     run_futs = []
     for i in range(int(args['repeats'])):
         print('\n\nRunning case: ' + str(i), flush = True)
+        
+        sbatch_options = SbatchOptions(
+            **slurm_args, 
+            job_name = f'run_mpi_hello_world_ompi_{i}',
+            output = f'run_mpi_hello_world_ompi_{i}.out',
+        )
+
         run_fut = run_mpi_hello_world_ompi(
-            args['srun_cmd'].replace('___', ' '),
+            sbatch_options.header,
             args['np'], 
             args['mpi_dir'],
             inputs = [compile_fut],
--- workflow_apps.py
+++ workflow_apps.py
@@ -1,12 +1,11 @@
 from parsl.app.app import bash_app
 import parsl_utils
 
-# PARSL APPS:
-
 
 @parsl_utils.parsl_wrappers.log_app
 @bash_app(executors=['myexecutor_1'])
-def compile_mpi_hello_world_ompi(ompi_dir, inputs = [], stdout='compile.out', stderr = 'compile.err'):
+def compile_mpi_hello_world_ompi(ompi_dir: str, inputs: list = None, 
+                                 stdout: str ='compile.out', stderr: str = 'compile.err'):
     """
     Creates the mpitest binary in the working directory
     """
@@ -24,18 +23,26 @@ def compile_mpi_hello_world_ompi(ompi_dir, inputs = [], stdout='compile.out', st
 
 @parsl_utils.parsl_wrappers.log_app
 @bash_app(executors=['myexecutor_1'])
-def run_mpi_hello_world_ompi(srun_cmd, np, ompi_dir, inputs = [], outputs = [], stdout='std.out', stderr = 'std.err'):
+def run_mpi_hello_world_ompi(sbatch_header: str, np: int, ompi_dir: str,
+                             inputs: list = None, outputs: list = None, 
+                             stdout: str ='std.out', stderr: str = 'std.err'):
     """
-    Runs the binary using the specified srun command and number of processors
+    Runs the binary using the specified srun command and number of processors.
+    NOTES:
+      - Using srun results in duplication of the output lines, see https://github.com/parallelworks/issues/issues/1102
     """
     return '''
+    cat > run_mpi_hello_world_ompi_{case}.sh <<EOF
+    #!/bin/bash
+    {sbatch_header}
+
     export OMPI_DIR={ompi_dir}
     export PATH=$OMPI_DIR/bin:$PATH
-    export LD_LIBRARY_PATH=$OMPI_DIR/lib:$LD_LIBRARY_PATH
-    export MANPATH=$OMPI_DIR/share/man:$MANPATH
-    {srun_cmd} mpirun -n {np} mpitest > {output}
+    mpirun -n {np} mpitest > {output}
+    EOF
+    sbatch -W run_mpi_hello_world_ompi_{case}.sh
     '''.format(
-        srun_cmd = srun_cmd,
+        sbatch_header = sbatch_header,
         np = np,
         ompi_dir = ompi_dir,
         output = outputs[0].local_path
