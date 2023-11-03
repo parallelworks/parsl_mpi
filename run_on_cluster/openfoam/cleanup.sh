94bf3d7886053ed70e5fbddbf4daf771adf7f661
Parallel Works app-run user <sfgary@mgmt-sfgary-cloud2-00078.c.modular-magpie-167320.internal>
2023-11-03 14:51:58 +0000

    Add cleanup script

---
+++ run_on_cluster/openfoam/cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#======================
+# Clean up all files
+# created while running
+# these scripts so
+# temporary files are not
+# committed to GitHub.
+#======================
+
+# Container
+rm -f openfoam.sif
+
+
