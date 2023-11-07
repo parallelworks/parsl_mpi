916c7cba8a4626edae4bc4ab2efc576789011b85
Parallel Works app-run user <sfgary@mgmt-sfgary-cloud2-00081.c.modular-magpie-167320.internal>
2023-11-07 03:27:11 +0000

    Change hello-world singularity case to match OpenFOAM default size

--- run_on_cluster/openfoam/step_04_run_container_hello.sh
+++ run_on_cluster/openfoam/step_04_run_container_hello.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --nodes=10
+#SBATCH --nodes=6
 #SBATCH --job-name=test-singularity-mpi
 #SBATCH --output=test-singularity-mpi.out
 #SBATCH --ntasks-per-node=2
 #SBATCH --partition=small
 
 # LOAD OpenMPI ENVIRONMENT HERE!
-source $HOME/parsl_mpi/run_on_cluster/openfoam/openmpi_env.sh
-#source /contrib/alvaro/ompi/env.sh
+#source $HOME/parsl_mpi/run_on_cluster/openfoam/openmpi_env.sh
+source /contrib/alvaro/ompi/env.sh
 
 # SET RUN DIR (same as #SBATCH --chdir=/path/to/OpenFOAM/case(
 RUN_DIR="$HOME"
@@ -18,7 +18,7 @@ SIF_PATH="$HOME/openfoam.sif"
 
 # SET NUMBER OF MPI PROCESSES
 # The max value is nodes x ntasks-per-node
-num_mpi_proc=20
+num_mpi_proc=12
 
 # Compile
 mpicc ${RUN_DIR}/parsl_mpi/mpitest.c
