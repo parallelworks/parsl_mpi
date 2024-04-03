f45440590cba7c28457d899829ff35b2a76ea64f
Parallel Works app-run user <sfgary@mgmt-sfgary-cloud2-00163.c.modular-magpie-167320.internal>
2024-04-03 13:48:02 +0000

    Clarify launch mechanisms with and without PMI

--- run_on_cluster/hello-world/install_scripts/step_00b_install_openmpi_no_Spack.sh
+++ run_on_cluster/hello-world/install_scripts/step_00b_install_openmpi_no_Spack.sh
@@ -23,9 +23,18 @@ export OMPI_URL="$OMPI_URL_PREFIX/v$OMPI_MAJOR_VERSION/openmpi-$OMPI_VERSION.tar
 export PATH=$OMPI_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$OMPI_DIR/lib:$LD_LIBRARY_PATH
 export MANPATH=$OMPI_DIR/share/man:$MANPATH
-# For SLURM-OpenMPI integration (i.e. launch via srun)
-export OMPI_SLURM_PMI_INCLUDE=/usr/include/slurm
-export OMPI_SLURM_PMI_LIBDIR=/usr/lib64
+# For SLURM-OpenMPI integration (i.e. launch via srun).
+# Requires slurm-devel for pmi<1|2|x>.h header files.
+# If you do not have access to these, you can still use
+# mpiexec with sbatch, but you won't be able to launch
+# MPI jobs directly with srun (see examples below).
+# To disable these config options, set with_pmi=false
+with_pmi=false
+if [ "$with_pmi" == "true" ]; then
+    echo "======> Setting OMPI_SLURM_PMI env vars..."
+    export OMPI_SLURM_PMI_INCLUDE=/usr/include/slurm
+    export OMPI_SLURM_PMI_LIBDIR=/usr/lib64
+fi
 export C_INCLUDE_PATH=/usr/include/slurm
 
 echo "===> Making /tmp/ompi work dir"
@@ -37,14 +46,32 @@ mkdir -p /tmp/ompi
 echo "===> Downloading OpenMPI v$OMPI_VERSION"
 cd /tmp/ompi && wget -O openmpi-$OMPI_VERSION.tar.gz $OMPI_URL && tar -xzf openmpi-$OMPI_VERSION.tar.gz
 
-echo "===> Compile and install OMPI"
+echo "===> Configure OMPI"
 # Allow for up to 30 concurrent compile jobs with -j
-cd /tmp/ompi/openmpi-$OMPI_VERSION && ./configure --prefix=$OMPI_DIR --with-flux-pmi --with-pmi=$OMPI_SLURM_PMI_INCLUDE --with-pmi-libdir=$OMPI_SLURM_PMI_LIBDIR && make install -j 30
+cd /tmp/ompi/openmpi-$OMPI_VERSION
+
+if [ "$with_pmi" == "true" ]; then
+    echo "======> with PMI"
+    ./configure --prefix=$OMPI_DIR --with-flux-pmi --with-pmi=$OMPI_SLURM_PMI_INCLUDE --with-pmi-libdir=$OMPI_SLURM_PMI_LIBDIR
+fi
+
+if [ "$with_pmi" == "false" ]; then
+    echo "======> without PMI"
+    ./configure --prefix=$OMPI_DIR
+fi
+
+echo "===> Compile and install OMPI"
+make install -j 30
 
 echo "Now that OpenMPI is installed, test it with the following steps:"
-echo "# Compile text code"
-echo "mpicc -o mpitest.out mpitest.c"
-echo "# Run test with PMI"
+echo " "
+echo "# Compile test code"
+echo "${OMPI_DIR}/bin/mpicc -o mpitest.out mpitest.c"
+echo " "
+echo "# Run test with PMI. You may not need the --mpi=pmi2 flag."
 echo "srun -N 2 -n 4 --mpi=pmi2 mpitest.out"
-echo "# Run test without PMI"
-echo "mpiexec -N 2 mpitest.out"
+echo " "
+echo "# Run test without PMI. mpiexec/mpirun are invoked within sbatch scripts!"
+echo "# Note that here, the mpiexec -N option specifies the number of tasks per"
+echo "# node, not the total number of tasks, which is what is in srun -n."
+echo "sbatch --output=tmp.std.out --exclusive --nodes=2 --wrap \"${OMPI_DIR}/bin/mpiexec -N 2 a.out\""
