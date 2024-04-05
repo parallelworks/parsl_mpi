c901314472f04a2f0a80b6da2397c2562f1af113
Parallel Works app-run user <sfgary@mgmt-sfgary-cloud2-00164.c.modular-magpie-167320.internal>
2024-04-05 17:27:41 +0000

    Change out the workflow launcher with NO-OP
    This is currently done for testing with the
    new Action launcher. There is no need for
    parsl_utils infrastructure yet.

---
+++ simple_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#==============================
+# Simple workflow launcher for
+# testing purposes.
+#
+# This launcher doesn't actually
+# do anything other than print
+# out its inputs.
+#==============================
+
+echo Hello, starting workflow!
+
+date
+
+env > env.dump
+
+echo Done with workflow!
+
