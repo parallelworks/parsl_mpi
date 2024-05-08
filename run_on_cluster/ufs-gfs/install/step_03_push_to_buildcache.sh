b4cbcc45b613012617ca9d08828a8018747c9fe0
Parallel Works app-run user <sfgary@mgmt-sfgary-aws1-00122.pw-canary-us-east-2-2.pw.local>
2024-05-08 22:52:17 +0000

    Automate two stage spack buildcache push

--- run_on_cluster/ufs-gfs/install/step_03_push_to_buildcache.sh
+++ run_on_cluster/ufs-gfs/install/step_03_push_to_buildcache.sh
@@ -4,15 +4,48 @@
 # the buildcache.
 #=============================
 
-# You need to activate spack first
-# and add the mirror. The local name
-# for the mirror ufs-cache.
+echo DO NOT RUN THIS SCRIPT WITH A SPACK
+echo ENVIRONMENT ALREADY SET!
+echo "=========================================="
+spack_install=${HOME}/spack/spack-stack
 mirror_name="ufs-cache"
+spack_env="ufs-weather-model.my-linux"
+echo "spack install: ${spack_install}"
+echo "mirror name:   ${mirror_name}"
+echo "spack_env: ${spack_env}"
+
+# This works in two steps:
+# 1) grab the packages from the "base Spack" (in particular compilers)
+# 2) grab the packages from the build environment.
+
+#=====================================================
+#Part 1: base Spack
+source ${spack_install}/setup.sh
+
+# This mirror has already been added
+# in step 2.
 
 for ii in $(spack find --format "yyy {version} /{hash}" |
 	    grep -v -E "^(develop^master)" |
 	    grep "yyy" |
 	    cut -f3 -d" ")
+do
+  echo Working on $ii
+  spack buildcache create -af --only=package --unsigned ${mirror_name} $ii
+done
+
+# After a push, you need to:
+spack buildcache update-index ${mirror_name}
+
+#===================================================
+# Part 2: the build environment
+cd ${spack_install}/envs/${spack_env}
+spack env activate -p .
+
+for ii in $(spack find --format "yyy {version} /{hash}" |
+            grep -v -E "^(develop^master)" |
+            grep "yyy" |
+            cut -f3 -d" ")
 do
   echo Working on $ii
   spack buildcache create -af --only=package --unsigned ${mirror_name} $ii
