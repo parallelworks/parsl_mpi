55dd3a8a2b2af16565e104c7fad67dc0f86c5469
Parallel Works app-run user <lobielodan@mgmt-lobielodan-gputesting-00027.c.modular-magpie-167320.internal>
2024-07-09 16:21:51 +0000

    saving entire model

--- run_on_cluster/cvae-weather-ensemble/make_graph.py
+++ run_on_cluster/cvae-weather-ensemble/make_graph.py
@@ -11,8 +11,13 @@ for history in history_files:
     history_df = pd.read_csv('./model_dir/' + history)
     all_history = pd.concat([all_history, history_df], ignore_index = True)
     
-
-plt.ylim(365, 400)
+    
+plt.ylim(365, 500)
 plt.plot(all_history['loss'])
+    
+# for i in range(0, len(all_history), 80):
+#     plt.plot(i, all_history['loss'].iloc[i], color = "red")
+
+
 plt.show()
 plt.savefig('plot_of_loss.png') 
