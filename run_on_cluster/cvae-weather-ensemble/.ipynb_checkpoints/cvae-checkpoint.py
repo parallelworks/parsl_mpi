2b3378674fa2db9a788702376304de75a17ff9cb
Parallel Works app-run user <lobielodan@mgmt-lobielodan-cvaetraining-00017.pw-canary-us-east-1.pw.local>
2024-07-29 17:24:27 +0000

    updates

--- run_on_cluster/cvae-weather-ensemble/.ipynb_checkpoints/cvae-checkpoint.py
+++ run_on_cluster/cvae-weather-ensemble/.ipynb_checkpoints/cvae-checkpoint.py
@@ -1,6 +1,6 @@
 # https://keras.io/examples/generative/vae/
 import os, json
-# import pickle
+import netCDF4
 import glob
 
 import numpy as np
@@ -8,12 +8,136 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
-from tf import keras
+from tensorflow import keras
 from keras import layers
 
 from sklearn.model_selection import train_test_split
 
 
+
+
+# model defintions ----------------------------------------------------------------------------------------------
+
+class Sampling(layers.Layer):
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape = (batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+def build_encoder(latent_dim):
+    encoder_inputs = keras.Input(shape=(721, 1440, 1))
+    
+    x = layers.Conv2D(32, 11, activation="relu", strides=[9, 10], padding="valid")(encoder_inputs)
+    x = layers.Conv2D(64, [5,9], activation="relu", strides=[5, 9], padding="valid")(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(16, activation="relu")(x)
+    
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    
+    print(encoder.summary())
+    return encoder
+
+def build_decoder(latent_dim):
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    
+    x = layers.Dense(15 * 15 * 64, activation="relu")(latent_inputs)
+    x = layers.Reshape((15, 15, 64))(x)
+    # FIXME - there is something wrong here, but at least there is a pattern.
+    # Using output_padding as a fudge factor -> it may be that there is exactly
+    # one "missing" filter stamp/convolution because for both Conv2DTranspose
+    # operations, output_padding is set to maximum it could be in both dims
+    # (i.e. exactly one less than the stride of each filter).
+    x = layers.Conv2DTranspose(64, [5, 9], activation="relu", strides=[5,9], padding="valid", output_padding=[4, 8])(x)
+    x = layers.Conv2DTranspose(32, 11, activation="relu", strides=[9,10], padding="valid", output_padding=[8, 9])(x)
+    
+    decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    
+    print(decoder.summary())
+    return decoder
+
+class VAE(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.total_loss_tracker = keras.metrics.Mean(name = "total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(name = "reconstruction_loss")
+        self.kl_loss_tracker = keras.metrics.Mean(name = "kl_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+
+    def train_step(self, data):
+        
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            # FIXME: Normalize loss with the number of features (28 * 28)
+            n_features = 28 * 28
+            reconstruction_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.binary_crossentropy(data, reconstruction), axis = (1, 2)
+                )
+            ) / n_features
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis = 1)) / n_features
+            total_loss = (reconstruction_loss + kl_loss)
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+
+    # Needed to validate (validation loss) and to evaluate
+    def test_step(self, data):
+        if type(data) == tuple:
+            data, _ = data
+            
+        z_mean, z_log_var, z = self.encoder(data)
+        reconstruction = self.decoder(z)
+        # FIXME: Normalize loss with the number of features (28 * 28)
+        n_features = 28 * 28
+        reconstruction_loss = tf.reduce_mean(
+            tf.reduce_sum(
+                keras.losses.binary_crossentropy(data, reconstruction), axis = (1, 2)
+            )
+        ) / n_features
+        kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+        kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis = 1)) / n_features
+        total_loss = (reconstruction_loss + kl_loss)
+        # grads = tape.gradient(total_loss, self.trainable_weights)
+        # self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+
+# ---------------------------------------------------------------------------------------------------------------
 
 # CNN require huge amounts of RAM, specially during training
 # Computational requirements for a single CNN layer:
@@ -156,33 +280,9 @@ def plot_images(images, rows, columns, path = '', show = False):
         plt.show()
     if path:
         plt.savefig(path)
-    
-
-def load_gefs_open_data_registry(dataset):
-    import xarray as xr
-    slp_01_ds = xr.open_mfdataset(dataset, engine='cfgrib')
-    print(slp_01_ds)
-    # Use MNIST data preproc as template
-    many_time_steps = False
-    if many_time_steps:
-        slp = np.expand_dims(
-            np.concatenate(
-                [
-                    slp_01_ds.PRES_P1_L1_GLL0.values,
-                    slp_02_ds.PRES_P1_L1_GLL0.values
-                ],
-                axis=0
-            ),
-            -1
-        ).astype("float16") / 110000
-    else:
-        slp = np.expand_dims(
-            slp_01_ds.PRES_P1_L1_GLL0.values,
-            -1).astype("float16") / 110000
-    return slp
-
-
-
+        
+        
+        
 
 # Maybe reconstruction loss is big because it is not normalized?
 if __name__ == '__main__':
