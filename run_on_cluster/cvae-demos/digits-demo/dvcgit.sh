7fd1e681c90d2c8abcc6d50d6b9225e3ae945c1b
Parallel Works app-run user <lobielodan@mgmt-lobielodan-cvaetraining-00031.pw-canary-us-east-1.pw.local>
2024-08-09 13:09:55 +0000

    updates

--- run_on_cluster/cvae-demos/digits-demo/dvcgit.sh
+++ run_on_cluster/cvae-demos/digits-demo/dvcgit.sh
@@ -3,7 +3,7 @@
 # Start Conda and select env (you will need to cross-check the paths and env name)
 
 source ~/pw/.miniconda3c/etc/profile.d/conda.sh
-conda activate cvae_env
+conda activate digit_env
 
 # Run the git commands using input from the command line;
 # $1 stores the first entry on the command line that launches
