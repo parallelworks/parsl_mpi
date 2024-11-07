2980947d0ae94caef95763863961790e4b67753d
Parallel Works app-run user <sfgary@mgmt-sfgary-gce2-00009.c.modular-magpie-167320.internal>
2024-11-07 14:50:00 +0000

    Add example for how to access MPI in Spack

--- run_on_cluster/openfoam/main_env.sh
+++ run_on_cluster/openfoam/main_env.sh
@@ -10,12 +10,31 @@
 # Custom shared attached storage:
 export OPENFOAM_SHARED_DIR=/openfoam
 
-# Select whether to use OpenMPI or IntelMPI
+#=====================================
+# Select which MPI to use
 # Pick ONE of the lines below
-source ${HOME}/parsl_mpi/run_on_cluster/openfoam/openmpi_env.sh
-#source /opt/intel/oneapi/setvars.sh
+#=====================================
+#
+# Custom OpenMPI installed locally
+# (You will normally need to build it yourself!)
+#source ${HOME}/parsl_mpi/run_on_cluster/openfoam/openmpi_env.sh
+#
+# Use this if you install Intel OneAPI directly
+# to the system, e.g. sudo yum install intel-hpckit
+# source /opt/intel/oneapi/setvars.sh
+#
+# Use this if you want to use IntelMPI from a
+# Spack installation. The path here assumes you
+# have already defined a SPACK_ROOT (e.g. in your 
+# ~/.bashrc).
+# This source line should be already in /etc/bashrc
+#source ${SPACK_ROOT}/share/spack/setup-env.sh
+# So you already have direct access to MPI in Spack:
+spack load intel-oneapi-mpi
 
+#=====================================
 # Specify the configuration of OpenFOAM
+#=====================================
 source openfoam_env.sh
 
 # Done!
