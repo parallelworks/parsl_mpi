4759d86910ad13cd13f69cd9a29342e3646f760b
StefanGary <stefanfgary@gmail.com>
2025-03-17 16:35:25 +0000

    Add rocoto installer

---
+++ run_on_cluster/ufs-gfs/install/step_01b_sys_install_rocoto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=======================
+# Install rocoto on the system
+#=======================
+
+# Grab repo
+pushd $HOME
+git clone https://github.com/christopherwharrop/rocoto.git
+
+# Run installer
+cd rocoto
+./INSTALL
+
+
